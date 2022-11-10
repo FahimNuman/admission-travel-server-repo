98752f4809269d64c957b4f3e7a0cf9cd8690fdb
Md.Abdullah Al Numan Fahim <fahiomnuman87@gmail.com>
2022-11-10 08:48:04 +0600

    added edit

--- index.js
+++ index.js
@@ -96,6 +96,21 @@ async function run(){
             res.send(result);
         })
 
+        app.put('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const review = req.body;
+            const option = { upsert: true };
+            const updatedReview = {
+                $set: {
+                    review: review.review
+                    
+                }
+            }
+            const result = await reviewCollection.updateOne(filter, updatedReview, option);
+            res.send(result);
+        })
+
         app.delete('/reviews/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
